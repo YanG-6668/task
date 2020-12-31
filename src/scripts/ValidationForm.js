cf8c8693a2a8b6e24eaa6239a51aa98e872a5b18
YanG-6668 <goch.yannn@gmail.com>
2020-12-31 12:26:15 +0200

    added vf-class

--- src/scripts/ValidationForm.js
+++ src/scripts/ValidationForm.js
@@ -1,97 +1,21 @@
 export default class ValidationForm {
   constructor(selector, rules) {
     this.form = document.querySelector(selector);
-    this.firstName = document.querySelector('#firstName');
-    this.lastName = document.querySelector('#lastName');
-    this.phone = document.querySelector('#phone');
-    this.email = document.querySelector('#email');
-    this.password = document.querySelector('#password');
-
-    // firstName start
-    this.minLengthFirstName = rules.firstName.minLength;
-    this.maxLengthFirstName = rules.firstName.maxLength;
-    // firstName end
-    // lastName start
-    this.minLengthLastName = rules.lastName.minLength;
-    this.maxLengthLastName = rules.lastName.maxLength;
-    // lastName end
-    //password start
-    this.minLengthPassword = rules.password.minLength;
-    this.maxLengthPassword = rules.password.maxLength;
-    //password end
+    this.rules = rules;
+    this.validCount = 0;
 
     this.setup();
   }
 
-  checkFirstName() {
-    const firstNameValue = this.firstName.value.trim();
-
-    if (!this.isRequired(firstNameValue)) {
-      this.errorMessage(this.firstName, 'First name cannot be empty!')
-    } else if (!this.betweenBorder(firstNameValue.length, this.minLengthFirstName, this.maxLengthFirstName)) {
-      this.errorMessage(this.firstName, `First name must be between ${this.minLengthFirstName} and ${this.maxLengthFirstName} characters.`);
-    } else {
-      this.successMessage(this.firstName);
-    }
-  }
-
-  checkLastName() {
-    const lastNameValue = this.lastName.value.trim();
-
-    if (!this.isRequired(lastNameValue)) {
-      this.errorMessage(this.lastName, 'Last name cannot be empty!')
-    } else if (!this.betweenBorder(lastNameValue.length, this.minLengthLastName, this.maxLengthLastName)) {
-      this.errorMessage(this.lastName, `Last name must be between ${this.minLengthLastName} and ${this.maxLengthLastName} characters.`);
-    } else {
-      this.successMessage(this.lastName);
-    }
-  }
-
-  checkPhone() {
-    const phoneValue = this.phone.value.trim();
-    const numberOfDigits = 14;
-
-    if (!this.isRequired(phoneValue)) {
-      this.errorMessage(this.phone, 'Phone cannot be empty!')
-    } else if (!this.testPhone(phoneValue) || phoneValue.length > numberOfDigits) {
-      this.errorMessage(this.phone, 'Example: +380732225551')
-    } else {
-      this.successMessage(this.phone);
-    }
-  }
-
-  checkEmail() {
-    const emailValue = this.email.value.trim();
-
-    if (!this.isRequired(emailValue)) {
-      this.errorMessage(this.email, 'Email cannot be empty!')
-    } else if (!this.testEmail(emailValue)) {
-      this.errorMessage(this.email, 'Example: example@.gmail.com')
-    } else {
-      this.successMessage(this.email);
-    }
-  }
-
-  checkPassword() {
-    const passwordValue = this.password.value.trim();
-
-    if (!this.isRequired(passwordValue)) {
-      this.errorMessage(this.password, 'Password cannot be empty!')
-    } else if (!this.betweenBorder(passwordValue.length, this.minLengthPassword, this.maxLengthPassword)) {
-      this.errorMessage(this.password, `Last name must be between ${this.minLengthPassword} and ${this.maxLengthPassword} characters.`)
-    } else if (!this.testPassword(passwordValue)) {
-      this.errorMessage(this.password, 'Example: abcd4')
-    } else {
-      this.successMessage(this.password);
-    }
-  }
-
-  isRequired(value) {
-    return value === '' ? false : true;
-  }
-
-  betweenBorder(valueLength, valueMin, valueMax) {
-    return valueLength > valueMin && valueLength < valueMax ? true : false;
+  checkInputs() {
+    this.rules.map(rule => {
+      if (rule.required) {
+        this.testInputs(rule.reg, rule.selector.value, rule.selector, rule.errorMessage);
+      } else {
+        this.successMessage(rule.selector)
+      }
+      console.log(this.validCount);
+    });
   }
 
   errorMessage(input, message) {
@@ -108,41 +32,23 @@ export default class ValidationForm {
     letter.innerText = 'Success';
   }
 
-  testEmail(email) {
-    const regexp = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-    return regexp.test(email);
-  }
-
-  testPassword(password) {
-    const regexp = /(?=[#$-/:-?{-~!"^_`\[\]a-zA-Z]*([0-9#$-/:-?{-~!"^_`\[\]]))(?=[#$-/:-?{-~!"^_`\[\]a-zA-Z0-9]*[a-zA-Z])[#$-/:-?{-~!"^_`\[\]a-zA-Z0-9]/;
-    return regexp.test(password);
-  }
-
-  testPhone(phone) {
-    const regexp = /[0-9]/;
-    return regexp.test(phone);
+  testInputs(reg, value, selector, errorMessage) {
+    if(reg.test(value)) {
+      this.successMessage(selector)
+      this.validCount++;
+    }else {
+      this.errorMessage(selector, errorMessage);
+    }
   }
 
   setup() {
-    this.form.addEventListener('input', e => {
-      const id = e.target.id
-      switch (id) {
-        case 'firstName': this.checkFirstName(); break;
-        case 'lastName': this.checkLastName(); break;
-        case 'phone': this.checkPhone(); break;
-        case 'email': this.checkEmail(); break;
-        case 'password': this.checkPassword(); break;
-      }
-    });
-
     this.form.addEventListener('submit', e => {
-      e.preventDefault();
+      this.checkInputs();
 
-      this.checkFirstName();
-      this.checkLastName();
-      this.checkPhone();
-      this.checkEmail();
-      this.checkPassword();
+      if(this.validCount !== this.rules.length) {
+        e.preventDefault();
+        this.validCount = 0;
+      } 
     });
   }
 }
--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,17 +1,44 @@
 import ValidationForm from './ValidationForm'
 'use strict';
 
-const vf = new ValidationForm('form', {
-  firstName: {
-    minLength: 3,
-    maxLength: 25
+const vf = new ValidationForm('form', [
+  {
+    selector: document.querySelector('#firstName'),
+    required: true,
+    reg: /\w{3,20}/,
+    // minLength: 3,
+    // maxLength: 15,
+    errorMessage: `You have to write something (minimum characters: 3 and maximum characters: 20)`,
   },
-  lastName: {
-    minLength: 3,
-    maxLength: 25
+  {
+    selector: document.querySelector('#lastName'),
+    required: true,
+    reg: /\w{3,20}/,
+    // minLength: 3,
+    // maxLength: 15,
+    errorMessage: `You have to write something (minimum characters: 3 and maximum characters: 20)`,
   },
-  password: {
-    minLength: 4,
-    maxLength: 8
+  {
+    selector: document.querySelector('#phone'),
+    required: true,
+    reg: /[0-9]{9,13}/,
+    // 'minLength': 3,
+    errorMessage: `You have to write phone and only numbers.`
+  },
+  {
+    selector: document.querySelector('#email'),
+    required: true,
+    reg: /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+    // 'minLength': 3,
+    // 'maxLength': 15
+    errorMessage: `You must write the correct email address. Example: joey.tribbiani@gmail.com`
+  },
+  {
+    selector: document.querySelector('#password'),
+    required: true,
+    reg: /(?=[#$-/:-?{-~!"^_`\[\]a-zA-Z]*([0-9#$-/:-?{-~!"^_`\[\]]))(?=[#$-/:-?{-~!"^_`\[\]a-zA-Z0-9]*[a-zA-Z])[#$-/:-?{-~!"^_`\[\]a-zA-Z0-9]{4,8}/,
+    // minLength: 4,
+    // maxLength: 8,
+    errorMessage: `You have to write password (this password must have letters, numbers, minimum characters: 4 and maximum characters: 8)`
   }
-});
+]);
